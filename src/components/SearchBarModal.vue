d2ae0b16cae6909b7d4f8d426781968cb498a93c
Himani Mehra <himanimehra528@gmail.com>
2023-12-15 17:26:02 +0530

    removed unwanted code

--- src/components/SearchBarModal.vue
+++ src/components/SearchBarModal.vue
@@ -1,185 +1,11 @@
-<!-- <script setup>
-import { defineProps, defineEmits, ref, onMounted } from 'vue';
-import axios from 'axios';
-
-const props = defineProps(['isVisible']);
-const emits = defineEmits();
-const tags = ref([]);
-const hoveredTagGroup = ref(null);
-
-
-const closeModal = () => {
-    emits('close');
-};
-
-
-onMounted(async () => {
-    try {
-        const response = await axios.get('https://api.acharyaprashant.org/v2/legacy/courses/tags');
-        tags.value = response;
-       
-
-
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-});
-const showSubDropdown = (tagGroup) => {
-    hoveredTagGroup.value = tagGroup;
-};
-
-</script>
-
-<template>
-    <div class="modal" @click="closeModal">
-        <div class="modal-content" @click.stop>
-            <div class="dropdown-container">
-                <div class="dropdown main-dropdown">
-                    <div class="main-dropdown-item" @mouseover="showSubDropdown(null)">
-                        All
-                    </div>
-                    <div v-for="(innerArray, outerIndex) in tags" :key="outerIndex">
-                        <div v-for="(element, innerIndex) in innerArray[0]" :key="innerIndex" class="display-flex">
-                            <div class="main-dropdown-item"
-                                @mouseover="element.hasChildren ? showSubDropdown(innerArray) : showSubDropdown(null)">
-                                {{ element.name.english }} {{ element.tagId }}
-                            </div>
-
-                            <div class="arrow" v-if="element.hasChildren">
-                                <img src="../assets/gray-arrow.png" alt="gray arrow" class="gray-arrow" />
-                                <img src="../assets/orange-arrow.png" alt="orange arrow" class="orange-arrow" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div v-if="hoveredTagGroup" class="dropdown sub-dropdown">
-                    <div v-for="(innerArray, outerIndex) in tags" :key="outerIndex">
-                        <div v-for="(element, innerIndex) in innerArray[1]" :key="innerIndex" class="display-flex">
-                           <div v-if="element.parent == 3">
-                            <div class="main-dropdown-item">
-                                {{ element.name.english }}
-                            </div>
-                           </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</template>
-
-<style scoped>
-.modal {
-    cursor: pointer;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 999;
-}
-
-.modal-content {
-    background-color: #fff;
-    padding: 20px;
-    border-radius: 8px;
-    z-index: 1000;
-}
-
-.dropdown-container {
-    display: flex;
-}
-
-.dropdown {
-    max-height: 300px;
-    overflow-y: auto;
-}
-
-.main-dropdown {
-    flex: 1;
-}
-
-.sub-dropdown {
-    flex: 1;
-}
-
-.display-flex {
-    display: flex;
-    align-items: center;
-    color: #ee7a3e;
-}
-
-.arrow {
-    position: relative;
-    width: 20px;
-    height: 20px;
-    display: inline-block;
-}
-
-.arrow img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    transition: opacity 0.3s ease, display 0s 0.3s;
-}
-
-.arrow .gray-arrow {
-    position: absolute;
-    top: 0;
-    left: 0;
-    opacity: 1;
-    z-index: 1;
-}
-
-.arrow .orange-arrow {
-    position: absolute;
-    top: 0;
-    left: 0;
-    opacity: 0;
-    z-index: 2;
-}
-
-.arrow:hover .gray-arrow {
-    opacity: 0;
-    display: none;
-}
-
-.arrow:hover .orange-arrow {
-    opacity: 1;
-}
-
-.main-dropdown-item {
-    font-weight: 700;
-    color: #515151;
-}
-
-.main-dropdown-item:hover {
-    color: #ee7a3e;
-}
-
-.main-dropdown-item,
-.sub-dropdown-item {
-    padding: 8px;
-    cursor: pointer;
-}
-</style> -->
-
-
-  
 <script>
 export default {
     data() {
         return {
-            apiUrl: 'https://api.acharyaprashant.org/v2/legacy/courses/tags', // Replace with your API endpoint
+            apiUrl: 'https://api.acharyaprashant.org/v2/legacy/courses/tags',
             hierarchicalStructure: [],
         };
     },
-
-
     methods: {
         fetchData() {
             fetch(this.apiUrl)
@@ -201,7 +27,6 @@ export default {
                         });
                     });
 
-                    // Convert the map to an array for rendering in the template
                     this.hierarchicalStructure = Object.values(level0Map);
                 })
                 .catch(error => console.error('Error fetching data from the API:', error));
@@ -220,6 +45,9 @@ export default {
                 }
             });
         },
+        shouldShowArrow(parentItem) {
+      return parentItem.children.some(child => child.parent === parentItem.tagId);
+    },
         closeModal() {
             this.$emit('close');
         },
@@ -230,26 +58,34 @@ export default {
 };
 </script>
 
-  
 <template>
     <div class="modal" @click="closeModal">
-        <div class="modal-content" @click.stop>
-            <div class="main-dropdown-item">
-                All
-            </div>
-            <div class="dropdown-container main-dropdown-item" v-for="item in hierarchicalStructure" :key="item.tagId"
-                @mouseenter="showChildren(item.tagId)" @mouseleave="hideChildren(item.tagId)">
-                {{ item.name }}
-                <div v-if="item.showChildren" class="main-dropdown-item">
-                    <div v-for="child in item.children" :key="child.tagId">
-                        {{ child.name }}
-                    </div>
-                </div>
-            </div>
+      <div class="modal-content" @click.stop>
+        <div class="main-dropdown-item">
+          All
         </div>
+        <div
+          class="dropdown-container main-dropdown-item"
+          v-for="item in hierarchicalStructure"
+          :key="item.tagId"
+          @mouseenter="showChildren(item.tagId)"
+          @mouseleave="hideChildren(item.tagId)"
+        >
+          {{ item.name }}
+          <div class="arrow" v-if="shouldShowArrow(item)">
+            <img src="../assets/gray-arrow.png" alt="gray arrow" class="gray-arrow" />
+            <img src="../assets/orange-arrow.png" alt="orange arrow" class="orange-arrow" />
+          </div>
+          <div v-if="item.showChildren" class="dropdown-children">
+            <div class="main-dropdown-item" v-for="child in item.children" :key="child.tagId">
+              {{ child.name }}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</template>
-  
+  </template>
+
 <style scoped>
 .modal {
     cursor: pointer;
@@ -338,7 +174,6 @@ export default {
     font-weight: 700;
     color: #515151;
 }
-
 .main-dropdown-item:hover {
     color: #ee7a3e;
 }
@@ -348,4 +183,17 @@ export default {
     padding: 8px;
     cursor: pointer;
 }
+
+
+
+
+
+
+
+
+  .dropdown-children {
+    max-height: 200px; /* Set the maximum height you want */
+    overflow-y: auto; /* Enable vertical scrolling when content overflows */
+  }
+
 </style>
